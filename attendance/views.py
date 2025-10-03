270a1fc99239981f321edcd8f6d13f4a1202b9ff
mohdib205 <miibm232005@gmail.com>
2025-10-03 17:18:33 +0530

    gittt2

--- attendance/views.py
+++ attendance/views.py
@@ -15,7 +15,6 @@ import holidays
 from director.views import send_whatsapp_message
 
 
-
 #payload for MultipleAttendance
 # {
 #     "teacher": 1,
@@ -42,7 +41,8 @@ class MultipleAttendanceViewSet1(ModelViewSet):
             return Response({"error": "Invalid date format. Use YYYY-MM-DD."}, status=status.HTTP_400_BAD_REQUEST)
 
         # ========================= NEW:24/09/25 ==========================
-#132 from sms-admin-technohub/saqibali2)         # Prevent attendance on Sundays
+
+         # Prevent attendance on Sundays
         if marked_at.weekday() == 6:
             return Response({"error": "Attendance cannot be marked on Sunday."}, status=status.HTTP_400_BAD_REQUEST)
 
