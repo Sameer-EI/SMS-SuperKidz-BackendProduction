c2d6e5663b10089c8fa667a5549a43a82a1295ac
Farha Alam <farha786alam@gmail.com>
2025-10-03 10:37:58 +0530

    added teacher status

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -34,7 +34,7 @@ class TeacherSerializer(serializers.ModelSerializer):
         model = Teacher
         fields = [
             'id', 'first_name', 'middle_name', 'last_name', 'password', 'email',
-            'phone_no', 'gender', 'adhaar_no', 'pan_no', 'qualification', 'user_profile','joining_date'
+            'phone_no', 'gender', 'adhaar_no', 'pan_no', 'qualification', 'user_profile','joining_date','is_active'
         ]
 
     def create(self, validated_data):
