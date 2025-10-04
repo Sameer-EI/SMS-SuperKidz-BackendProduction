cb67e0240276b690fc166984bf419aff90706866
Farha Alam <farha786alam@gmail.com>
2025-10-04 17:55:30 +0530

    import utils in attendance views

--- attendance/views.py
+++ attendance/views.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+from utils.email_notifications import send_email_notification
 from .models import  *
 from .serializers import *
 from django.utils.dateformat import format as date_format
